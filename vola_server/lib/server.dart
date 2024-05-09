2768c46436239808837769bd65d4362d96ec5f12
vonmartiner <martiner.thomas@yahoo.de>
2024-05-09 21:08:58 +0200

    password reset_mail_includet

--- vola_server/lib/server.dart
+++ vola_server/lib/server.dart
@@ -1,3 +1,5 @@
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
 import 'package:serverpod/serverpod.dart';
 
 import 'package:vola_server/src/web/routes/root.dart';
@@ -22,15 +24,56 @@ void run(List<String> args) async {
   // If you are using any future calls, they need to be registered here.
   // pod.registerFutureCall(ExampleFutureCall(), 'exampleFutureCall');
 
+  // Configuration for sign in with email.
   auth.AuthConfig.set(auth.AuthConfig(
     sendValidationEmail: (session, email, validationCode) async {
-      // TODO: integrate with mail server
-      print('Validation code: $validationCode');
+      // Retrieve the credentials
+      final gmailEmail = session.serverpod.getPassword('gmailEmail')!;
+      final gmailPassword = session.serverpod.getPassword('gmailPassword')!;
+
+      // Create a SMTP client for Gmail.
+      final smtpServer = gmail(gmailEmail, gmailPassword);
+
+      // Create an email message with the validation code.
+      final message = Message()
+        ..from = Address(gmailEmail)
+        ..recipients.add(email)
+        ..subject = 'Verification code for Serverpod'
+        ..html = 'Your verification code is: $validationCode';
+
+      // Send the email message.
+      try {
+        await send(message, smtpServer);
+      } catch (_) {
+        // Return false if the email could not be sent.
+        return false;
+      }
+
       return true;
     },
     sendPasswordResetEmail: (session, userInfo, validationCode) async {
-      // TODO: integrate with mail server
-      print('Validation code: $validationCode');
+      // Retrieve the credentials
+      final gmailEmail = session.serverpod.getPassword('gmailEmail')!;
+      final gmailPassword = session.serverpod.getPassword('gmailPassword')!;
+
+      // Create a SMTP client for Gmail.
+      final smtpServer = gmail(gmailEmail, gmailPassword);
+
+      // Create an email message with the password reset link.
+      final message = Message()
+        ..from = Address(gmailEmail)
+        ..recipients.add(userInfo.email!)
+        ..subject = 'Password reset link for Serverpod'
+        ..html = 'Here is your password reset code: $validationCode>';
+
+      // Send the email message.
+      try {
+        await send(message, smtpServer);
+      } catch (_) {
+        // Return false if the email could not be sent.
+        return false;
+      }
+
       return true;
     },
   ));
